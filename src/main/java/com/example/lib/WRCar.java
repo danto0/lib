65c35f48c91c693f70bc30cd3458066a3897994b
dantolino <david.antolinorivas@igt.com>
2023-05-15 16:02:08 +0200

    vehicle rename

---
+++ src/main/java/com/example/lib/WRCar.java
@@ -0,0 +1,18 @@
+package com.example.lib;
+
+public class WRCar {
+    private int numWheels;
+
+    public WRCar(int numWheels) {
+        this.numWheels = numWheels;
+    }
+
+    public void setNumWheels(int numWheels) {
+        this.numWheels = numWheels;
+    }
+
+    public int getNumWheels() {
+        return numWheels;
+    }
+
+}
